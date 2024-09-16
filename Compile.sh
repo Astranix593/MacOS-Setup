0cbc93e4eec236fe26b6a05fe9b791f2f9085225
Dashtiss <brandontisserand@outlook.com>
2024-09-16 12:17:00 -0500

    Added Compile

---
+++ Compile.sh
@@ -0,0 +1,9 @@
+# Will compile the DownloadAll.sh script
+URL="https://github.com/Astranix593/MacOS-Setup/raw/main/DownloadAll.sh"
+echo "Compiling DownloadAll.sh"
+rm -r Build
+mkdir Build
+# shc Usage: shc [-e date] [-m addr] [-i iopt] [-x cmnd] [-l lopt] [-o outfile] [-rvDSUHCABh] -f script
+shc -m $URL -f DownloadAll.sh -o Build/DownloadAll
+
+sudo codesign -f -s - -v Build/DownloadAll
